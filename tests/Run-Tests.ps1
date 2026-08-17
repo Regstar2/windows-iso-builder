fc01458fd790a25a66f2147ec15ea746bbb52d1c
Regstar2 <carkov195@gmail.com>
2026-08-17 10:04:27 +0300

    pass repository root to Pester static tests

--- tests/Run-Tests.ps1
+++ tests/Run-Tests.ps1
@@ -6,4 +6,17 @@ $ErrorActionPreference = 'Stop'
 if (-not (Get-Module -ListAvailable -Name Pester)) {
     throw 'Pester не установлен. Установите Pester 5 и повторите запуск.'
 }
-Invoke-Pester -Path $PSScriptRoot -Output Detailed
+
+$previousRepositoryRoot = $env:WIB_REPOSITORY_ROOT
+try {
+    $env:WIB_REPOSITORY_ROOT = Split-Path -Parent $PSScriptRoot
+    Invoke-Pester -Path $PSScriptRoot -Output Detailed
+}
+finally {
+    if ($null -eq $previousRepositoryRoot) {
+        Remove-Item Env:WIB_REPOSITORY_ROOT -ErrorAction SilentlyContinue
+    }
+    else {
+        $env:WIB_REPOSITORY_ROOT = $previousRepositoryRoot
+    }
+}
