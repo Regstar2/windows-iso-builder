e4721390490247e2430fb4db18089f2b72bb0971
Regstar2 <carkov195@gmail.com>
2026-08-20 10:15:41 +0300

    Add ComboBox dropdown interaction regression

---
+++ tests/WindowsISOBuilder.Gui.Tests/ComboBoxTemplateTests.cs
@@ -0,0 +1,71 @@
+using System.Reflection;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+namespace WindowsISOBuilder.Gui.Tests;
+
+[TestClass]
+public sealed class ComboBoxTemplateTests
+{
+    [TestMethod]
+    public void DarkComboBoxTemplateKeepsDropdownInteractive()
+    {
+        Exception? failure = null;
+        var completed = false;
+
+        var thread = new Thread(() =>
+        {
+            try
+            {
+                var resources = new ResourceDictionary
+                {
+                    Source = new Uri("/WindowsISOBuilder;component/Resources/ComboBoxStyles.xaml", UriKind.Relative)
+                };
+
+                var style = resources[typeof(ComboBox)] as Style;
+                Assert.IsNotNull(style);
+
+                var comboBox = new ComboBox
+                {
+                    Style = style,
+                    Width = 220
+                };
+                comboBox.Items.Add("One");
+                comboBox.Items.Add("Two");
+                comboBox.SelectedIndex = 0;
+                comboBox.ApplyTemplate();
+
+                var toggle = comboBox.Template.FindName("PART_DropDownToggle", comboBox) as ToggleButton;
+                Assert.IsNotNull(toggle);
+                Assert.IsFalse(comboBox.IsDropDownOpen);
+
+                var onClick = typeof(ToggleButton).GetMethod("OnClick", BindingFlags.Instance | BindingFlags.NonPublic);
+                Assert.IsNotNull(onClick);
+                onClick.Invoke(toggle, null);
+
+                Assert.IsTrue(comboBox.IsDropDownOpen, "The themed ComboBox toggle must open the dropdown.");
+                comboBox.IsDropDownOpen = false;
+            }
+            catch (Exception ex)
+            {
+                failure = ex;
+            }
+            finally
+            {
+                completed = true;
+            }
+        });
+
+        thread.SetApartmentState(ApartmentState.STA);
+        thread.Start();
+        Assert.IsTrue(thread.Join(TimeSpan.FromSeconds(10)), "The WPF ComboBox interaction test timed out.");
+        Assert.IsTrue(completed);
+
+        if (failure is not null)
+        {
+            Assert.Fail(failure.ToString());
+        }
+    }
+}
